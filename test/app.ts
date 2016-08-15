d7bc15c3eebfeabfbeb0e2c89749b2496f85fd0f
IOAyman <theycallmethedr@gmail.com>
2016-08-15 09:49:00 +0100

    Fixed implicit any type

--- test/app.ts
+++ test/app.ts
@@ -1,13 +1,12 @@
-
 import express = require('express');
 import router from './router';
 
 const app: express.Application = express();
 
 // requesthandler
-const requestHandler: express.RequestHandler = (req, res, next) => {
+const requestHandler: express.RequestHandler = (req: express.Request, res: express.Response, next: express.NextFunction) => {
     console.log(req.ip);
-    res.json({hello: 'world'});
+    res.json({ hello: 'world' });
     req.app.get('some_setting');
     next(new Error());
 };
